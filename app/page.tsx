361dfeab4e165f6033aaf143e0abbc7863ed4af4
MaxoGitAcc <maxowork19@gmail.com>
2025-09-28 22:30:19 +0400

    Initial commit: my Next.js portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Contact from '@/components/Contact';
-import Navbar from '@/components/Navbar';
-import About from '@/components/layout/About';
-import Hero from '@/components/layout/Hero'
-import Projects from '@/components/layout/Projects'
-import Skills from '@/components/layout/Skills';
+import Contact from '../components/Contact';
+import Navbar from '../components/Navbar';
+import About from '../components/layout/About';
+import Hero from '../components/layout/Hero'
+import Projects from '../components/layout/Projects'
+import Skills from '../components/layout/Skills';
 
 export default function Home() {
   return (
